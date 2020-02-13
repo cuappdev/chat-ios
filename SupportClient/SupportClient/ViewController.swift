44ed6fd9842a140f1585850e37540ebab665ac2c
Omar Rasheed <oyr2@cornell.edu>
2020-02-12 20:51:58 -0500

    Final shittems

---
+++ SupportClient/SupportClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SupportClient
+//
+//  Created by Omar Rasheed on 2/15/20.
+//  Copyright © 2020 Cornell Appdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
