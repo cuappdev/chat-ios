da9a806f4c906a9112baa2adeb6e078b222e2885
Cameron Russell <camrussell215@gmail.com>
2020-02-16 01:14:34 -0500

    Add necessary constants

---
+++ SupportClient/SupportClient/Utilities/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  SupportClient
+//
+//  Created by Cameron Russell on 2/15/20.
+//  Copyright © 2020 Cornell Appdev. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    
+    static let backgroundColor = UIColor.white
+    static let themeColor = UIColor(red: 85/255, green: 212/255, blue: 212/255, alpha: 1)
+    static let navigationTintColor = UIColor(white: 237/255, alpha: 1)
+    static let titleColor = UIColor(white: 64/255, alpha: 1)
+    static let subtitleColor = UIColor(white: 159/255, alpha: 1)
+    
+    static let robotoMedium = "Roboto-Medium"
+    static let robotoRegular = "Roboto-Regular"
+    
+}
