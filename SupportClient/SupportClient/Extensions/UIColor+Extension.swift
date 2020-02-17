4cf33b7f3d519d1fdcda30f527c0d97b84aa459e
Cameron Russell <camrussell215@gmail.com>
2020-02-16 21:36:45 -0500

    Add UIColor and Font Extensions, Delete Constants

---
+++ SupportClient/SupportClient/Extensions/UIColor+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIColor+Extension.swift
+//  SupportClient
+//
+//  Created by Cameron Russell on 2/16/20.
+//  Copyright © 2020 Cornell Appdev. All rights reserved.
+//
+
+import Foundation
