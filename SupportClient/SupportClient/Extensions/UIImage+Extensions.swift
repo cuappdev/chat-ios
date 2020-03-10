df54907d63f28925d4dff6384f5b1a201fd1bb47
Cameron Russell <camrussell215@gmail.com>
2020-03-09 23:20:26 -0400

    Add border to UIImage with padding

--- SupportClient/SupportClient/Extensions/UIImage+Extensions.swift
+++ SupportClient/SupportClient/Extensions/UIImage+Extensions.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2020 Cornell Appdev. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+
+    func addPadding(x: CGFloat, y: CGFloat) -> UIImage? {
+        let width = size.width + x
+        let height = size.height + y
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: width, height: height), false, 0)
+        let origin = CGPoint(x: x/2, y: y/2)
+        draw(at: origin)
+        let imageWithPadding = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return imageWithPadding
+    }
+    
+}
