fb67aadcd6ca2f04a17dddf7f07bf74639e584f4
Cameron Russell <camrussell215@gmail.com>
2020-03-09 23:19:35 -0400

    Add image configuration and resizing ratio

---
+++ SupportClient/SupportClient/Extensions/UIImage+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIImage+Extensions.swift
+//  SupportClient
+//
+//  Created by Cameron Russell on 3/9/20.
+//  Copyright © 2020 Cornell Appdev. All rights reserved.
+//
+
+import Foundation
--- SupportClient/SupportClient/Views/ErrorFileCollectionViewCell.swift
+++ SupportClient/SupportClient/Views/ErrorFileCollectionViewCell.swift
@@ -7,33 +7,55 @@
 //
 
 import UIKit
-import SnapKit
+import Photos
 
 class ErrorFileCollectionViewCell: UICollectionViewCell {
     
     static let reuseID = "ErrorFileCollectionViewCell"
+    static let imageResizingRatio: CGFloat = 1.7
+
+    private let imageManager = PHCachingImageManager()
+    private var errorFileImageView: UIImageView!
     
-    var errorFileImageView: UIImageView!
+    private let padding: CGFloat = 20.0
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        errorFileImageView = UIImageView()
-        errorFileImageView.contentMode = .scaleAspectFit
-        contentView.addSubview(errorFileImageView)
+        setupImageView()
         setupContraints()
     }
     
-    func setupContraints() {
-        errorFileImageView.snp.makeConstraints { make in
-            make.leading.equalTo(contentView.snp.leading)
-            make.trailing.equalTo(contentView.snp.trailing)
-            make.top.equalTo(contentView.snp.top).inset(50)
-            make.bottom.equalTo(contentView.snp.bottom).inset(-50)
-        }
+    func setupImageView() {
+        errorFileImageView = UIImageView()
+        errorFileImageView.translatesAutoresizingMaskIntoConstraints = false
+        errorFileImageView.contentMode = .scaleAspectFit
+        errorFileImageView.layer.borderWidth = 1.0
+        errorFileImageView.layer.borderColor = UIColor.black.cgColor
+        errorFileImageView.layer.cornerRadius = 10.0
+        contentView.addSubview(errorFileImageView)
     }
     
-    func configure(for imageFile: UIImage) {
-        errorFileImageView.image = imageFile
+    func setupContraints() {
+        NSLayoutConstraint.activate([
+            errorFileImageView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / ErrorFileCollectionViewCell.imageResizingRatio + padding / 2.0),
+            errorFileImageView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / ErrorFileCollectionViewCell.imageResizingRatio),
+            errorFileImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+        ])
+    }
+    
+    // TODO: Figure out why short pause when loading images after dismissing ImagePicker
+    func configure(for imageFile: PHAsset) {
+        // Converts a PHAsset to a UIImage and configures the cell's image
+        imageManager.requestImage(
+            for: imageFile,
+            targetSize: CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height),
+            contentMode: .aspectFit,
+            options: nil,
+            resultHandler: { image, _ in
+                let imageWithPadding = image?.addPadding(x: self.padding, y: 2.0 * self.padding)
+                self.errorFileImageView.image = imageWithPadding
+            }
+        )
     }
     
     required init?(coder: NSCoder) {
