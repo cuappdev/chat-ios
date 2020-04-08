b7a2f3d55738e81d06de53566acff931fde8e9b4
Cameron Russell <54007865+cammmmm@users.noreply.github.com>
2020-04-08 17:59:16 -0400

    [1/3] Create Feedback Form (#10)
    * Add necessary fonts and icons
    
    * Add SnapKit pod and configure plist
    
    * Add necessary constants
    
    * Add landing page and feedback tableview
    
    * Add UIColor and Font Extensions, Delete Constants
    
    * Make Feedback conform to Codable protocol
    
    * Fix UIColor options
    
    * Streamline and refactor landing page
    
    * Rename file for consistency
    
    * Formatting and newline fixes
    
    * Add DZNEmptyDataSet pod
    
    * Add animations and update constraints
    
    * Fix formatting
    
    * Fix comment formatting
    
    * Conform to DZNEmptyDataSet
    
    * Add new feedback form with collection view
    
    * Change privacy settings
    
    * Add BSImagePicker for multiple image selection
    
    * Create feedback form UI
    
    * Fix image picker life cycle issue
    
    * Add image configuration and resizing ratio
    
    * Add border to UIImage with padding
    
    * Improve code consistency and update collection view
    
    * Add NSLayoutConstraints and make PR changes
    
    * The death of SnapKit
    
    * Fix animation and views
    
    * PR fixes and CustomView abstraction
    
    * Fixes button tapping inside custom view
    
    * Fix bug with button functionality
    
    * Make PR changes
    
    Co-authored-by: Omar Rasheed <oyr2@cornell.edu>
    Co-authored-by: Kevin Chan <kevinchan159@gmail.com>

---
+++ SupportClient/SupportClient/Controllers/FeedbackViewController.swift
@@ -0,0 +1,163 @@
+//
+//  FeedbackViewController.swift
+//  SupportClient
+//
+//  Created by Cameron Russell on 3/6/20.
+//  Copyright © 2020 Cornell Appdev. All rights reserved.
+//
+
+import BSImagePicker
+import DZNEmptyDataSet
+import Photos
+import UIKit
+
+class FeedbackViewController: UIViewController {
+
+    private var attachedErrorFiles = [PHAsset]()
+    private var collectionView: UICollectionView!
+    
+    private let messageLabel = UILabel()
+    private let messageTextField = UITextField()
+    private let typeLabel = UILabel()
+                
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .backgroundColor
+        setupNavigationBar()
+        setupCollectionView()
+        setupConstraints()
+    }
+    
+    func setupNavigationBar() {
+        navigationController?.navigationBar.barTintColor = .navigationTintColor
+        let attributes = [
+            NSAttributedString.Key.font: UIFont._21RobotoMedium!,
+            NSAttributedString.Key.foregroundColor: UIColor.titleColor
+        ]
+        let attributedTitle = NSAttributedString(string: "New Feedback", attributes: attributes)
+        title = attributedTitle.string
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            title: "Cancel",
+            style: .plain,
+            target: self,
+            action: #selector(handleNavigationBarLeftTap)
+        )
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Send",
+            style: .plain,
+            target: self,
+            action: #selector(handleNavigationBarRightTap)
+        )
+    }
+    
+    func setupCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.emptyDataSetSource = self
+        collectionView.emptyDataSetDelegate = self
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .backgroundColor
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.register(ErrorFileCollectionViewCell.self, forCellWithReuseIdentifier: ErrorFileCollectionViewCell.reuseID)
+        view.addSubview(collectionView)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / ErrorFileCollectionViewCell.imageResizingRatio),
+            collectionView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+        ])
+    }
+    
+    // TODO: animation does not work after presenting ImagePicker sometimes
+    @objc func handleNavigationBarLeftTap() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func handleNavigationBarRightTap() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
+
+// MARK: - UICollectionView DataSource
+extension FeedbackViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return attachedErrorFiles.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ErrorFileCollectionViewCell.reuseID, for: indexPath) as! ErrorFileCollectionViewCell
+        let errorFile = attachedErrorFiles[indexPath.item]
+        cell.configure(for: errorFile)
+        return cell
+    }
+
+}
+
+// MARK: - UICollectionView Delegate
+extension FeedbackViewController: UICollectionViewDelegate {
+    
+    // TODO: Respond to user taps to open image editor for markup
+    
+}
+
+// MARK: - UICollectionView DelegateFlowLayout
+extension FeedbackViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let scaledScreenWidth = view.layer.bounds.width / ErrorFileCollectionViewCell.imageResizingRatio
+        let scaledScreenHeight = view.layer.bounds.height / ErrorFileCollectionViewCell.imageResizingRatio
+        return CGSize(width: scaledScreenWidth, height: scaledScreenHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let collectionViewImagePadding = (UIScreen.main.bounds.width - UIScreen.main.bounds.width / ErrorFileCollectionViewCell.imageResizingRatio) / 2.0
+        return UIEdgeInsets(top: 0, left: collectionViewImagePadding, bottom: 0, right: collectionViewImagePadding)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 40
+    }
+
+}
+
+// MARK: - DZNEmptyDataSet
+extension FeedbackViewController: DZNEmptyDataSetSource, DZNEmptyDataSetDelegate {
+    
+    // If no screenshots/videos are added by the user, they are prompted to add files
+    func customView(forEmptyDataSet scrollView: UIScrollView!) -> UIView! {
+        return
+            AddImageView(
+                onPress: {
+                    let imagePicker = ImagePickerController()
+                    imagePicker.settings.theme.selectionStyle = .checked
+                    imagePicker.settings.fetch.assets.supportedMediaTypes = [.image, .video]
+                    self.presentImagePicker(imagePicker,
+                        select: nil,
+                        deselect: nil,
+                        cancel: nil,
+                        finish: { assets in
+                            self.attachedErrorFiles = assets.map { $0 }
+                            self.collectionView.reloadData()
+                        },
+                        completion: nil
+                    )
+                }
+            )
+    }
+    
+    func emptyDataSetShouldAllowScroll(_ scrollView: UIScrollView) -> Bool {
+        return false
+    }
+    
+    func emptyDataSetShouldAllowTouch(_ scrollView: UIScrollView) -> Bool {
+        return true
+    }
+    
+}
--- SupportClient/SupportClient/Controllers/ViewController.swift
+++ SupportClient/SupportClient/Controllers/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import SnapKit
 import DZNEmptyDataSet
 
 class ViewController: UIViewController {
@@ -22,7 +21,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.backgroundColor        
+        view.backgroundColor = .backgroundColor
         setupData()
         setupNavigationBar()
         setupFeedbackTableView()
@@ -42,13 +41,13 @@ class ViewController: UIViewController {
         """
         
         guard let jsonData = jsonString.data(using: .utf8) else { return }
-        let feedback = try! JSONDecoder().decode(Feedback.self, from: jsonData)
+        let _ = try! JSONDecoder().decode(Feedback.self, from: jsonData)
         
-        feedbackData = [feedback]
+        feedbackData = []
     }
     
     func setupNavigationBar() {
-        navigationController?.navigationBar.barTintColor = UIColor.navigationTintColor
+        navigationController?.navigationBar.barTintColor = .navigationTintColor
         let attributes = [
             NSAttributedString.Key.font: UIFont._21RobotoMedium!,
             NSAttributedString.Key.foregroundColor: UIColor.titleColor
@@ -57,13 +56,14 @@ class ViewController: UIViewController {
         title = attributedTitle.string
         navigationItem.rightBarButtonItem = UIBarButtonItem(
             image: UIImage(named: "pen"),
-            style: UIBarButtonItem.Style.plain,
+            style: .plain,
             target: self,
             action: #selector(handleNavigationBarRightTap)
         )
     }
     
     func setupFeedbackTableView() {
+        feedbackTableView.translatesAutoresizingMaskIntoConstraints = false
         feedbackTableView.delegate = self
         feedbackTableView.dataSource = self
         feedbackTableView.tableFooterView = UIView()
@@ -74,11 +74,14 @@ class ViewController: UIViewController {
     }
     
     func setupConstraints() {
-        feedbackTableView.snp.makeConstraints { make  in
-            make.leading.trailing.top.bottom.equalToSuperview()
-        }
+        NSLayoutConstraint.activate([
+            feedbackTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            feedbackTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            feedbackTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            feedbackTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
-    
+        
     @objc func handleNavigationBarRightTap() {
         let isEvenNumTaps = countEditTaps % 2 == 0
         feedbackTableView.setEditing(isEvenNumTaps, animated: true)
@@ -93,7 +96,6 @@ extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 75
     }
-    
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let _ = feedbackTableView.cellForRow(at: indexPath) as! FeedbackTableViewCell
@@ -132,67 +134,12 @@ extension ViewController: UITableViewDataSource {
 extension ViewController: DZNEmptyDataSetSource, DZNEmptyDataSetDelegate {
     
     func customView(forEmptyDataSet scrollView: UIScrollView!) -> UIView! {
-        
-        let customView = UIView()
-
-        // Create ParagraphStyle to format label text
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.alignment = .center
-        paragraphStyle.lineSpacing = 10
-        
-        // Tell user there is no feedback to display
-        let feedbackLabel = UILabel()
-        feedbackLabel.lineBreakMode = .byWordWrapping
-        feedbackLabel.numberOfLines = 0
-        let title = "No Feedback Yet\n"
-        let subtitle = "See feedback conversations here"
-        let titleAttributes: [NSAttributedString.Key : Any] = [
-            NSAttributedString.Key.font: UIFont._17RobotoMedium!,
-            .paragraphStyle: paragraphStyle,
-            NSAttributedString.Key.foregroundColor: UIColor.titleColor
-        ]
-        let attributedText = NSMutableAttributedString(string: title, attributes: titleAttributes)
-        let subtitleAttributes = [
-            NSAttributedString.Key.font: UIFont._13RobotoRegular!,
-            NSAttributedString.Key.foregroundColor: UIColor.subtitleColor
-        ]
-        attributedText.append(NSAttributedString(string: subtitle, attributes: subtitleAttributes))
-        feedbackLabel.attributedText = attributedText
-        feedbackLabel.sizeToFit()
-        
-        // Start new conversation button
-        let newConversationButton = UIButton()
-        newConversationButton.setTitle("Start Conversation", for: .normal)
-        newConversationButton.backgroundColor = UIColor.themeColor
-        newConversationButton.titleLabel?.font = UIFont._17RobotoMedium
-        newConversationButton.layer.cornerRadius = 22
-        
-        // Add elements to UIView
-        customView.addSubview(newConversationButton)
-        customView.addSubview(feedbackLabel)
-        
-        // Set up constraints
-        feedbackLabel.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview().offset(-60)
-        }
-        
-        newConversationButton.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(feedbackLabel.snp.bottom).offset(view.frame.height / 2.5)
-            make.width.equalToSuperview().multipliedBy(0.6)
-            make.height.equalTo(45)
-        }
-        
-        // Adds animation
-        newConversationButton.alpha = 0
-        feedbackLabel.alpha = 0
-        UIView.transition(with: customView, duration: 1.25, animations: {
-            newConversationButton.alpha = 1
-            feedbackLabel.alpha = 1
-        }, completion: nil)
-
-        return customView
+        return NoMessageView(
+            onPress: {
+                let feedbackViewController = UINavigationController(rootViewController: FeedbackViewController())
+                self.present(feedbackViewController, animated: true, completion: nil)
+            }
+        )
     }
     
     func emptyDataSetShouldAllowScroll(_ scrollView: UIScrollView) -> Bool {
