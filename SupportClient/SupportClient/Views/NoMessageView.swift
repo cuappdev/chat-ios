7807b8da3102d0e3f4b4cebf23a93e20dce7e814
Cameron Russell <camrussell215@gmail.com>
2020-04-23 15:22:02 -0400

    Remove old files

--- SupportClient/SupportClient/Views/NoMessageView.swift
+++ SupportClient/SupportClient/Views/NoMessageView.swift
@@ -11,11 +11,15 @@ import UIKit
 /**
  UIView for when user has no current two-way communication channels open
  */
-class NoMessageView: CustomView {
+class NoMessageView: UIView {
             
     private let feedbackLabel = UILabel()
     private let newConversationButton = UIButton()
     private var onPress: (() -> Void)?
+    
+    override var intrinsicContentSize: CGSize {
+        return UIScreen.main.bounds.size
+    }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
