d8c1d159b02d25fd302113eab27172b5a0266b8a
Cameron Russell <54007865+cammmmm@users.noreply.github.com>
2020-05-06 14:02:56 -0400

    Delete NSMutableAttributedString+Extensions.swift

--- SupportClient/SupportClient/Extensions/NSMutableAttributedString+Extensions.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UILabel+Extensions.swift
-//  SupportClient
-//
-//  Created by Cameron Russell on 5/4/20.
-//  Copyright © 2020 Cornell Appdev. All rights reserved.
-//
-
-import Foundation
