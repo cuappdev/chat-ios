7df0daa436b8935f4de8965cedfbd8cc2fce8df3
Omar Rasheed <oyr2@cornell.edu>
2020-05-21 03:58:15 -0700

    Reorganize repo

---
+++ SupportClient/Extensions/UIFont+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  UIFont.swift
+//  SupportClient
+//
+//  Created by Cameron Russell on 2/16/20.
+//  Copyright © 2020 Cornell Appdev. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    static let _13RobotoMedium = UIFont(name: "Roboto-Medium", size: 13)
+    static let _17RobotoMedium = UIFont(name: "Roboto-Medium", size: 17)
+    static let _21RobotoMedium = UIFont(name: "Roboto-Medium", size: 21)
+
+    static let _13RobotoRegular = UIFont(name: "Roboto-Regular", size: 13)
+    static let _17RobotoRegular = UIFont(name: "Roboto-Regular", size: 17)
+    static let _21RobotoRegular = UIFont(name: "Roboto-Regular", size: 21)
+
+}
+
